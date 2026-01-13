e393d0b973675ed9c81b0f00643c936827b77d7f
BORGEAS-J-MPOMPA <mpo23002@byui.edu>
2026-01-13 23:55:50 +0100

    Added changes

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+
 public static class Arrays
 {
     /// <summary>
@@ -8,12 +10,25 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
+        // Set a condition to return an empty array if the length is less than or egal to zero
+        if (length <= 0)
+            return Array.Empty<double>();
+            // Assign result to new double depending on the length
+        double[] result = new double[length];
+
+        // Iterate through the function
+
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = number * (i + 1);
+        }
+        
         // TODO Problem 1 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        return result; // replace this return statement with your own
     }
 
     /// <summary>
@@ -25,6 +40,31 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
+        // Create a variable to count data in the list
+        int n = data.Count;
+        
+        // Set a conditional function returns nothing if the list or the array is empty
+        if (n == 0)
+            return;
+
+        // Set a normal rotation of the amount
+        amount %= n;
+
+        // Create a temporary list or array 
+        List<int> temp = new List<int>(new int[n]);
+        
+        // Put each element in its rotated position
+        for (int i = 0; i < n; i++)
+        {
+            int newIndex = (i + amount) % n;
+            temp[newIndex] = data[i];
+        }
+
+        // Copy the rotated values back
+        for (int i = 0; i < n; i++)
+        {
+            data[i] = temp[i];
+        }
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
